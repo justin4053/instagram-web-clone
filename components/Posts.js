1063fbf13333943538fb3241c50db523b90c5f04
justin <jusin4053@gmail.com>
2022-03-15 14:12:34 +0800

    14:12

--- components/Posts.js
+++ components/Posts.js
@@ -3,32 +3,9 @@ import Post from "./Post"
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
 import { db } from "../firebase"
 
-// const posts = [
-//   {
-//     id: "123",
-//     username: "justin1",
-//     userImg: "https://img.ltn.com.tw/Upload/news/600/2022/02/27/74.jpg",
-//     img: "https://img.ltn.com.tw/Upload/news/600/2022/02/27/74.jpg",
-//     caption: "AAAAAABBBBBBBBBBCCCCCCCCCCCCC",
-//   },
-//   {
-//     id: "123",
-//     username: "justin2",
-//     userImg: "https://img.ltn.com.tw/Upload/news/600/2022/02/27/74.jpg",
-//     img: "https://img.ltn.com.tw/Upload/news/600/2022/02/27/74.jpg",
-//     caption: "AAAAAABBBBBBBBBBCCCCCCCCCCCCC",
-//   },
-//   {
-//     id: "123",
-//     username: "justin3",
-//     userImg: "https://img.ltn.com.tw/Upload/news/600/2022/02/27/74.jpg",
-//     img: "https://img.ltn.com.tw/Upload/news/600/2022/02/27/74.jpg",
-//     caption: "AAAAAABBBBBBBBBBCCCCCCCCCCCCC",
-//   },
-// ]
-
 function Posts() {
   const [posts, setPosts] = useState([])
+
   useEffect(
     () =>
       onSnapshot(
